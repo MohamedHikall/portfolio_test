931475d4491d5a11b6bfca33a64f59da337a527d
MohamedHikall <hikal1545@gmail.com>
2025-03-28 06:57:44 +0200

    upload new design resume

---
+++ js/script.js
@@ -0,0 +1,13 @@
+  var typed = new Typed(".multiple-text", {
+    strings: ['Frontend Developer', 'Youtuber', 'Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+  });
+  var typed = new Typed(".about-text", {
+    strings: ['Frontend Developer!', 'Mobile Developer!', 'UI/UX Designer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
